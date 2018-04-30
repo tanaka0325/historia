7700ede3f3f7c0ee6d584a9542faa960f9e90eb5
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2018-04-30 13:26:45 +0900

    Add Page model

---
+++ app/models.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from app import db
+
+
+class Page(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String, nullable=False, unique=True)
+    title = db.Column(db.String)
+    is_read = db.Column(db.Boolean, nullable=False, default=0)
+    note = db.Column(db.Text)
+    score = db.Column(db.Integer, nullable=False, default=0)
+    created_at = db.Column(
+        db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Page {}>'.format(self.title)
