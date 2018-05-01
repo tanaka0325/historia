660d0bca0c3498365b4a2fada835087c4213a08b
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2018-05-01 20:43:50 +0900

    Change modal to function from class

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -5,38 +5,35 @@ interface ModalProps {
   isActive: boolean;
 }
 
-export class Modal extends React.Component<ModalProps, {}> {
-  closeModal = () => {
-    this.props.closeModal();
+export const Modal = (props: ModalProps) => {
+  const closeModal = () => {
+    props.closeModal();
   };
 
-  render() {
-    const activeModalClassName = this.props.isActive
-      ? 'modal is-active'
-      : 'modal';
-    return (
-      <div className={activeModalClassName}>
-        <div className="modal-background" onClick={this.closeModal} />
-        <div className="modal-card">
-          <header className="modal-card-head">
-            <p className="modal-card-title">Modal title</p>
-            <button
-              className="delete"
-              aria-label="close"
-              onClick={this.closeModal}
-            />
-          </header>
-          <section className="modal-card-body">
-            <p>aaaaaaaaaaaaa</p>
-          </section>
-          <footer className="modal-card-foot">
-            <button className="button is-success">Save changes</button>
-            <button className="button" onClick={this.closeModal}>
-              Cancel
-            </button>
-          </footer>
-        </div>
+  const activeModalClassName = props.isActive ? 'modal is-active' : 'modal';
+
+  return (
+    <div className={activeModalClassName}>
+      <div className="modal-background" onClick={this.closeModal} />
+      <div className="modal-card">
+        <header className="modal-card-head">
+          <p className="modal-card-title">Modal title</p>
+          <button
+            className="delete"
+            aria-label="close"
+            onClick={this.closeModal}
+          />
+        </header>
+        <section className="modal-card-body">
+          <p>aaaaaaaaaaaaa</p>
+        </section>
+        <footer className="modal-card-foot">
+          <button className="button is-success">Save changes</button>
+          <button className="button" onClick={this.closeModal}>
+            Cancel
+          </button>
+        </footer>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
