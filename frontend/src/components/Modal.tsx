b0a85f2f95193aa37421bdb30ebeb89a819843c9
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2018-05-01 21:21:42 +0900

    Fix

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -14,22 +14,18 @@ export const Modal = (props: ModalProps) => {
 
   return (
     <div className={activeModalClassName}>
-      <div className="modal-background" onClick={this.closeModal} />
+      <div className="modal-background" onClick={closeModal} />
       <div className="modal-card">
         <header className="modal-card-head">
           <p className="modal-card-title">Modal title</p>
-          <button
-            className="delete"
-            aria-label="close"
-            onClick={this.closeModal}
-          />
+          <button className="delete" aria-label="close" onClick={closeModal} />
         </header>
         <section className="modal-card-body">
           <p>aaaaaaaaaaaaa</p>
         </section>
         <footer className="modal-card-foot">
           <button className="button is-success">Save changes</button>
-          <button className="button" onClick={this.closeModal}>
+          <button className="button" onClick={closeModal}>
             Cancel
           </button>
         </footer>
